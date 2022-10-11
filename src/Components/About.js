1752e6df547c4a6cf9bb64b5e2f466b6d579dd61
Mashud05052001 <masudmahi0505@gmail.com>
2022-10-12 02:29:58 +0600

    about page font mini size making

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='text-center'>
-            <h1 className='text-8xl  mt-40'>Mashudur Rahman Mahi</h1>
+            <h1 className='text-4xl md:text-8xl  mt-40'>Mashudur Rahman Mahi</h1>
         </div>
     );
 };
